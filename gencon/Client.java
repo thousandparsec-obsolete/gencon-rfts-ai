436b6770236ad3d926490ffef637c5ba16889b72
Victor Ivri <victor.ivri@gmail.com>
2008-05-07 15:32:36 -0400

    some preliminary class stubs

---
+++ gencon/Client.java
@@ -0,0 +1,10 @@
+package gencon;
+
+import net.thousandparsec.netlib.*;
+import net.thousandparsec.netlib.tp03.*;
+
+public class Client 
+{
+	
+	
+}
---
+++ gencon/Runner.java
@@ -0,0 +1,14 @@
+package gencon;
+
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
