e031db14e42474655974109f1ecaad860d8b295b
Victor Ivri <victor.ivri@gmail.com>
2008-05-12 20:25:48 -0400

    Very basic client; successfully connects to server.

--- gencon/Client.java
+++ gencon/Client.java
@@ -1,10 +1,58 @@
 package gencon;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.concurrent.Future;
+
 import net.thousandparsec.netlib.*;
 import net.thousandparsec.netlib.tp03.*;
 
-public class Client 
+public class Client extends TP03Visitor 
 {
+	String connectionString;
+	Connection<TP03Visitor> conn;
 	
+	Client() {}
+	
+	void init(String conStr)
+	{
+		this.connectionString = conStr;
+	}
+	
+	/**
+	 * 
+	 * Runs the client.
+	 * 
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 * @throws InterruptedException
+	 * @throws TPException
+	 * 
+	 */
+	void run() throws UnknownHostException, IOException, URISyntaxException, InterruptedException, TPException
+	{
+		establishConnection();
+		
+		conn.close();
+	}
+	
+/**
+ * Establishes a connection with the server.
+ * 
+ * @throws UnknownHostException
+ * @throws IOException
+ * @throws URISyntaxException
+ * @throws InterruptedException
+ * @throws TPException
+ */
+	private void establishConnection() throws UnknownHostException, IOException, URISyntaxException, InterruptedException, TPException
+	{
+		TP03Decoder decoder = new TP03Decoder();
+		conn = decoder.makeConnection(new URI(connectionString), true, new TP03Visitor(false));
+		conn.addConnectionListener(new DefaultConnectionListener<TP03Visitor>());
+	}
 	
 }
--- gencon/Runner.java
+++ gencon/Runner.java
@@ -1,14 +1,44 @@
 package gencon;
 
+import java.io.*;
+import java.net.UnknownHostException;
+import java.util.*;
+
 public class Runner {
 
 	/**
+	 * The runner class of the AI client
 	 * @param args
 	 */
 	public static void main(String[] args) 
 	{
+		System.out.println("Genetic Conquest: An AI Client for Thousand Parsec : RFTS ruleset.");
 		
-
+		Scanner stin = new Scanner(System.in);
+		System.out.print("Enter the address of the server : ");
+		String server = stin.next();
+		System.out.print("Enter your username : ");
+		String usrname = stin.next();
+		System.out.print("Enter your password : ");
+		String pwd = stin.next();
+		
+		/* WORKING ASSUMPTION : 
+		 * the usr+address string is formatted thus:   
+		 * ~assumption taken from the formatting in TestConnect.java~ */
+		String connectionString = "tp://" + pwd + ":" + usrname + "@" + server;
+		
+		//starting up the client
+		Client client = new Client();
+		client.init(connectionString);
+		try
+		{
+			client.run();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
-
+	
 }
