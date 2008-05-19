6af0a961aa75215f1266e7b77f1fbada9776a303
Victor Ivri <victor.ivri@gmail.com>
2008-05-19 16:18:01 -0400

    Merged functionality of Runner into Client; added some features. Client still -very- basic.

--- gencon/Client.java
+++ gencon/Client.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Future;
 import net.thousandparsec.netlib.*;
 import net.thousandparsec.netlib.tp03.*;
 
-public class Client extends TP03Visitor 
+public class Client
 {
 	//maintanence
 	private String URIString;
@@ -23,33 +23,119 @@ public class Client extends TP03Visitor
 	//game-related
 	private int difficulty;
 	
-	Client() {}
+	/**
+	 * Runner method for the client.
+	 * 
+	 * @param args [0] -c : connect to a URI specified in [1]. Else if none given: proceed normally.
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Genetic Conquest: An AI Client for Thousand Parsec : RFTS ruleset.\n");
+		
+		if (args.length > 0 && args[0].equals("-c"))
+			new Client(args[1]);
+		else if (args.length == 0)
+			new Client();
+		else
+		{
+			System.out.print("Input error. Try again.");
+			System.exit(-1);
+		}
+	}
 	
 	/**
-	 * 
-	 * @param conStr the string that specifies the address of the server, the username, and the password
+	 * Starts a client without specified URI. Manual entry of URI components (server address, username, password)
+	 *
 	 */
-	void init(String UStr)
+	Client()
 	{
-		this.URIString = UStr;
+		init();
+	}
+	
+	/**
+	 * Starts a client with specified URI. For testing and autoconnect.
+	 * @param args same as in {@link main}
+	 */
+	Client(String URI) 
+	{
+		init(URI);
+	}
+	
+	/**
+	 * initializes client without specified URI string.
+	 *
+	 */
+	private void init()
+	{
+ 		stout.print("Enter the address of the server : ");
+		String server = stin.next();
+		
+		stout.println("Create new account or use existing? (enter: 'n' or 'e'");
+		
+		String login = stin.next();
+		if (login.equals("e"))
+		{
+			setURIfromStdinput(server);
+		}
+		else if (login.equals("n"))
+		{
+			//the first string in user[] is username, the second is password.
+			String[] user = createNewAccount(server);
+			setURI(user[0], user[1], server); 
+		}
+
+		//run client
+		run();
+	}
+	
+	/**
+	 * Initializes client with specified URI string.
+	 * @param URI the string that specifies the address of the server, the username, and the password
+	 */
+	private void init(String URI)
+	{
+		this.URIString = URI;
+		
+		//run client
+		run();
+	}
+	
+	private void setURIfromStdinput(String server)
+	{
+		stout.print("Enter username : ");
+		String usrname = stin.next();
+		stout.print("Enter password : ");
+		String pwd = stin.next();
+		
+		//the URI string is formatted thus:  
+		this.URIString = "tp://" + usrname + ":" + pwd + "@" + server;
+	}
+	
+	private void setURI(String usrname, String pwd, String server)
+	{
+		this.URIString = "tp://" + usrname + ":" + pwd + "@" + server;
 	}
 	
 	/**
 	 * 
-	 * Runs the client.
-	 * 
-	 * @throws UnknownHostException
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 * @throws InterruptedException
-	 * @throws TPException
-	 * 
+	 * @return String[] : index 0 is username, index 1 is password.
 	 */
-	void run()
+	private String[] createNewAccount(String server)
+	{
+		return null;
+	}
+	
+	/**
+	 * Runs the client.
+	 */
+	private void run()
 	{
 		//first establish a connection with the server
 		establishConnection();
-
+		
+		//set to default:
+		setDifficulty(5, false);
+		
 		//execute command-line orders, until encountering control character 'q'
 		stout.println("Enter command. 'list' shows all available commands.");	
 		
@@ -65,7 +151,6 @@ public class Client extends TP03Visitor
 				printCommands();
 			}
 			else if (command.equals("start")){
-				setDifficulty(5);
 				startPlay();
 			}
 			else if (command.length() == 6 && 
@@ -73,7 +158,7 @@ public class Client extends TP03Visitor
 					command.substring(5, 6).matches("[1-9]")){
 				try
 				{
-					setDifficulty(command.charAt(5));
+					setDifficulty(new Integer(command.substring(5,6)).intValue(), true);
 				}
 				catch (Exception e)
 				{
@@ -92,13 +177,13 @@ public class Client extends TP03Visitor
 	 */
 	private void printCommands()
 	{
-		stout.println("> The list of available commands <");
+		stout.println("\n> The list of available commands <");
 		stout.println("'q' - close connection, and exit client.");
 		stout.println("'list' - lists all available commands.");
-		stout.println("'start' - start playing the game. Default difficulty 5.");
+		stout.println("'start' - start playing the game. Default difficulty: 5");
 		stout.println("'diff arg' - set difficulty; arg = 1 --> 9");
 		
-		stout.println("> ------------------------------ <");
+		stout.println("> ------------------------------ <\n");
 		
 	}
 	
@@ -128,7 +213,7 @@ public class Client extends TP03Visitor
 			stout.print("Establishing connection to server... ");
 		
 			TP03Decoder decoder = new TP03Decoder();
-			conn = decoder.makeConnection(new URI(URIString), true, new TP03Visitor(true));
+			conn = decoder.makeConnection(new URI(URIString), true, new TP03Visitor(false));
 			DefaultConnectionListener<TP03Visitor> listener = new DefaultConnectionListener<TP03Visitor>();
 			conn.addConnectionListener(listener);
 		
@@ -143,13 +228,17 @@ public class Client extends TP03Visitor
 		}
 	}
 	
+	
 	/**
 	 * Sets the difficulty of the AI opponent.
 	 * @param diff between 1 --> 9.
+	 * @param verbose display or not.
 	 */
-	private void setDifficulty(int diff)
+	private void setDifficulty(int diff, boolean verbose)
 	{
 		this.difficulty = diff;
+		if (verbose)
+			stout.println("Difficulty set to " + this.difficulty + "/9");
 	}
 	
 	/**
@@ -188,7 +277,6 @@ public class Client extends TP03Visitor
 	 */
 	private void exit(String message)
 	{
-		//closing the connection, and shutting down the client.
 		try
 		{
 			stout.println("Exit: " + message);
