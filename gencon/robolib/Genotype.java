e8ad22856dad874a4dd55b2b5b15ca3cefc02988
Victor Ivri <victor.ivri@gmail.com>
2008-07-08 16:21:29 -0400

    added additional 'allele'.

--- gencon/robolib/Genotype.java
+++ gencon/robolib/Genotype.java
@@ -26,32 +26,33 @@ public class Genotype
 	 * 
 	 * Here's a complete rundown on the traits and their meaning:
 	 * 
-	 * 	////////////
+	 *	////////////
 		// "A_" Action behaviors:
 		////////////
 		
-		A_COLONIALIST1, // <TRAIT #0> (0) colonize own sector first ---- (1) colonize own and neighbouring non-hostile sectors first --- (2) colonize own and not-neighbouring non-hostile sectors first.
+		A_COLONIALIST1, // <TRAIT #0> (0) colonize own sector first ---- (1) colonize own and neighbouring non-hostile sectors --- (2) colonize own and not-neighbouring non-hostile sectors.
 		A_COLONIALIST2, // <TRAIT #1> (0) send colonizer without defensive fleet ---- (1) send colonizer with weak defensive fleet --- (2) send colonizer with strong defensive fleet.
 		
-		A_ATTACK1, // <TRAIT #2> (0) send army to bordering hostile sectors ---- (1) equal preference ---- (2) send army to enemy heartland.
-		A_ATTACK2, // <TRAIT #3> (0) send at least evenly matched army to hostile sectors --- (1) equal preference ---- (2) only send huge flotillas to crush enemy.
+		A_ATTACK1, // <TRAIT #2> (0) send army to bordering hostile sectors ---- (1) no preference ---- (2) send army to enemy heartland.
+		A_ATTACK2, // <TRAIT #3> (0) send at least evenly matched army to hostile sectors --- (1) no preference ---- (2) only send huge flotillas to crush enemy.
 		
 		////////////
 		// "E_" Economic behaviors:
 		////////////
 		
-		E_OVERALL, // <TRAIT #4> (0) prefer to develop economics over army ---- (1) equal preference ---- (2) prefer to emass forces instead of developing economics.
+		E_OVERALL, // <TRAIT #4> (0) prefer to develop economics over army ---- (1) no preference ---- (2) prefer to emass forces instead of developing economics.
 		
-		E_COLONIALIST1, // <TRAIT #5> (0) prefer to produce colonialist fleet in Periphery sectors ---- (1) equal preference --- (2) prefer to produce colonialist fleet in Stronghold sectors
-		E_COLONIALIST2, // <TRAIT #6> (0) prefer to produce colonialist fleet in non-threatened sectors ---- (1) equal preference ---- (2) prefern to produce colonialist fleet in threatened sectors.
+		E_COLONIALIST1, // <TRAIT #5> (0) produce few colonists/transports ---- (1) produce an intermediate quantity ---- (2) produce large quantity.
+		E_COLONIALIST2, // <TRAIT #6> (0) prefer to produce colonialist fleet in Periphery sectors ---- (1) no preference --- (2) prefer to produce colonialist fleet in Stronghold sectors
+		E_COLONIALIST3, // <TRAIT #7> (0) prefer to produce colonialist fleet in non-threatened sectors ---- (1) no preference ---- (2) prefern to produce colonialist fleet in threatened sectors.
 		
 		//the "develop economics" behaviors:
-		E_INDUSTRY, // <TRAIT #7> (0) industry unimportant ---- (1) normal importance ---- (2) very important.
-		E_SOCENV, // <TRAIT #8> (0) social-environment unimportant ---- (1) normal importance ---- (2) very important.
-		E_PLANENV, // <TRAIT #9> (0) planetary-environment unimportant ---- (1) normal importance ---- (2) very important.
-		E_POPMAINT, // <TRAIT #10> (0) population-maintanance unimportant ---- (1) normal importance ---- (2) very important.
+		E_INDUSTRY, // <TRAIT #8> (0) industry unimportant ---- (1) normal importance ---- (2) very important.
+		E_SOCENV, // <TRAIT #9> (0) social-environment unimportant ---- (1) normal importance ---- (2) very important.
+		E_PLANENV, // <TRAIT #10> (0) planetary-environment unimportant ---- (1) normal importance ---- (2) very important.
+		E_POPMAINT, // <TRAIT #11> (0) population-maintanance unimportant ---- (1) normal importance ---- (2) very important.
 		
-		E_RESEARCH, // <TRAIT #11> (0) ship-tech unimportant ---- (1) normal importance ---- (2) very important.
+		E_RESEARCH, // <TRAIT #12> (0) ship-tech unimportant ---- (1) normal importance ---- (2) very important.
 	 * 
 	 * 
 	 */
@@ -73,17 +74,17 @@ public class Genotype
 		
 		E_OVERALL, // <TRAIT #4> (0) prefer to develop economics over army ---- (1) no preference ---- (2) prefer to emass forces instead of developing economics.
 		
-		E_COLONIALIST1, // <TRAIT #5> (0) prefer to produce colonialist fleet in Periphery sectors ---- (1) no preference --- (2) prefer to produce colonialist fleet in Stronghold sectors
-		E_COLONIALIST2, // <TRAIT #6> (0) prefer to produce colonialist fleet in non-threatened sectors ---- (1) no preference ---- (2) prefern to produce colonialist fleet in threatened sectors.
+		E_COLONIALIST1, // <TRAIT #5> (0) produce few colonists/transports ---- (1) produce an intermediate quantity ---- (2) produce large quantity.
+		E_COLONIALIST2, // <TRAIT #6> (0) prefer to produce colonialist fleet in Periphery sectors ---- (1) no preference --- (2) prefer to produce colonialist fleet in Stronghold sectors
+		E_COLONIALIST3, // <TRAIT #7> (0) prefer to produce colonialist fleet in non-threatened sectors ---- (1) no preference ---- (2) prefern to produce colonialist fleet in threatened sectors.
 		
 		//the "develop economics" behaviors:
-		E_INDUSTRY, // <TRAIT #7> (0) industry unimportant ---- (1) normal importance ---- (2) very important.
-		E_SOCENV, // <TRAIT #8> (0) social-environment unimportant ---- (1) normal importance ---- (2) very important.
-		E_PLANENV, // <TRAIT #9> (0) planetary-environment unimportant ---- (1) normal importance ---- (2) very important.
-		E_POPMAINT, // <TRAIT #10> (0) population-maintanance unimportant ---- (1) normal importance ---- (2) very important.
+		E_INDUSTRY, // <TRAIT #8> (0) industry unimportant ---- (1) normal importance ---- (2) very important.
+		E_SOCENV, // <TRAIT #9> (0) social-environment unimportant ---- (1) normal importance ---- (2) very important.
+		E_PLANENV, // <TRAIT #10> (0) planetary-environment unimportant ---- (1) normal importance ---- (2) very important.
+		E_POPMAINT, // <TRAIT #11> (0) population-maintanance unimportant ---- (1) normal importance ---- (2) very important.
 		
-		E_RESEARCH, // <TRAIT #11> (0) ship-tech unimportant ---- (1) normal importance ---- (2) very important.
-		
+		E_RESEARCH, // <TRAIT #12> (0) ship-tech unimportant ---- (1) normal importance ---- (2) very important.
 	}
 	
 	
