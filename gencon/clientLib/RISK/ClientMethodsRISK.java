e8cf6c95da38dfd65366e848c826660e277710c9
Victor Ivri <victor.ivri@gmail.com>
2008-08-13 16:03:34 -0400

    Done implementing method for fetching all objects from server, and converting them.

--- gencon/clientLib/RISK/ClientMethodsRISK.java
+++ gencon/clientLib/RISK/ClientMethodsRISK.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 
 import net.thousandparsec.netlib.TPException;
 import net.thousandparsec.netlib.tp03.Object;
+import net.thousandparsec.netlib.tp03.ObjectParams;
+import net.thousandparsec.util.Pair;
 
 import gencon.clientLib.Client;
 import gencon.clientLib.ClientMethods;
@@ -27,11 +29,55 @@ public class ClientMethodsRISK extends ClientMethods
 	
 	private synchronized Collection<RiskGameObject> convertAllObjects(Collection<Object> objects)
 	{
-		Collection<RiskGameObject> riskObjects = new HashSet<RiskGameObject>(objects.size());
+		//arrange all objects by category:
+		Collection<Object> galaxies = new HashSet<Object>();
+		Collection<Object> wormholes = new HashSet<Object>();
+		Collection<Object> starSystems = new HashSet<Object>();
+		Collection<Object> planets = new HashSet<Object>();
 		
 		for (Object object : objects)
-			riskObjects.add(ObjectConverterRISK.convertObject(object));
+		{
+			if (object.getObject().getParameterType() == ObjectParams.Galaxy.PARAM_TYPE)
+				galaxies.add(object);
+			else if (object.getObject().getParameterType() == ObjectParams.Wormhole.PARAM_TYPE)
+				wormholes.add(object);
+			else if (object.getObject().getParameterType() == ObjectParams.StarSystem.PARAM_TYPE)
+				starSystems.add(object);
+			else if (object.getObject().getParameterType() == ObjectParams.Planet.PARAM_TYPE)
+				planets.add(object);
+		}
 		
+		//preparing cotainer of converted bodies
+		Collection<RiskGameObject> riskObjects = new HashSet<RiskGameObject>(objects.size());
+		
+		//converting galaxies and wormholes, and adding to the container:
+		for (Object galaxy : galaxies)
+			riskObjects.add(ObjectConverterRISK.convertConstellation(galaxy));
+		for (Object wormhole : wormholes)
+			riskObjects.add(ObjectConverterRISK.convertWormhole(wormhole));
+		
+		//pairing up star systems with planets:
+		Collection<Pair<Object, Object>> Star_pairs = new HashSet<Pair<Object,Object>>();
+		for (Object ss : starSystems)
+		{
+			int idContains = ss.getContains().get(0).getId();
+			
+			for (Object pl : planets)
+				if (pl.getId() == idContains)
+				{
+					Pair<Object, Object> star_pair = new Pair<Object, Object>(ss, pl); 
+					Star_pairs.add(star_pair); 
+					break;
+				}
+		}
+		assert starSystems.size() == planets.size(); //to make sure they are paired up correspondingly!
+		
+		//converting the star-pairs, and adding to the container:
+		for (Pair<Object, Object> pair : Star_pairs)
+			riskObjects.add(ObjectConverterRISK.convertStar(pair.left, pair.right));
+		
+		
+		//if this point is reached, then everything's fine.
 		return riskObjects;
 	}
 
